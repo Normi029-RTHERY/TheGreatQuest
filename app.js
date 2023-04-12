149613cc1afcc3f1c13ac4812a64bf6c20fc08b8
Normi029-RTHERY <rom.t95@free.fr>
2023-04-12 17:34:18 +0200

    made custom aframe font working, re-enabling non acsii caracteres

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ markers.forEach(e => {
 
 
 var i = 0;
-var demoMarker = 'Bonjour ! Je suis le noble tisserand, vous voulez vous aventurez pour la grande quete ? Mais vous etes nu.e comme un ver ! Ecoutez, j aurais besoin de materiaux pour faire une armure, trouvez les … et vous serez equipe.e !';
+var demoMarker = 'Te voilà enfin aventurier.ère ! Je me présente, je suis Bill, maître tisserand. Dites-moi mon brave, j’ai entendu dire que vous partiez explorer les contrées perdues de notre royaume de Serres mais de ce que je vois vous êtes nu.e comme un ver ! Écoutez, il a peut être une tunique que je peux vous fournir mais j’aurais besoin d’étoffes supplémentaires, aidez moi à les retrouvez et je vous équiperai !';
 var demoText = document.querySelector('#demo-text');
 
 var delay = 25;
