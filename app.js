ea50d4ed64db2c0ba1a4001d564d11fc7e3143b6
Normi029-RTHERY <rom.t95@free.fr>
2023-04-12 02:53:55 +0200

    added multiple text support

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 
-var marker = document.querySelector('#demo-marker');
-marker.addEventListener('markerFound', function () {
-  // execute your function here
-  typeWriter();
-  console.log('found');
+var markers = document.querySelectorAll('a-marker');
+markers.forEach(e => {
+  e.addEventListener('markerFound', function () {
+    typeWriter(e.id);
+    console.log('found' + e.id);
+  });
 });
 
 var i = 0;
-var txt = 'Bonjour ! Je suis le noble tisserand, vous voulez vous aventurez pour la grande quete ? Mais vous etes nu.e comme un ver ! Ecoutez, j aurais besoin de materiaux pour faire une armure, trouvez les … et vous serez equipe.e !';
-var speed = 25;
+var demoMarker = 'Bonjour ! Je suis le noble tisserand, vous voulez vous aventurez pour la grande quete ? Mais vous etes nu.e comme un ver ! Ecoutez, j aurais besoin de materiaux pour faire une armure, trouvez les … et vous serez equipe.e !';
+var delay = 25;
 var demoText = document.querySelector('#demo-text');
 
-function typeWriter() {
-  if (i < txt.length) {
-    console.log(demoText);
-    demoText.setAttribute('value', demoText.getAttribute('value') + txt.charAt(i));
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+
+
+
+function typeWriter(markerId) {
+  var txt = eval(markerId)
+  setInterval(() => {
+    if (i < txt.length) {
+      console.log(demoText);
+      demoText.setAttribute('value', demoText.getAttribute('value') + txt.charAt(i));
+      i++;
+    }
+  }, 25);
 }
