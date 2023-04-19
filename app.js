9dfae9997d925c80e49268478c07969a77613030
Normi029-RTHERY <rom.t95@free.fr>
2023-04-19 10:09:19 +0200

    ze

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 
 var markers = document.querySelectorAll('a-marker');
+var currentMarker = null;
 
 markers.forEach(e => {
   e.addEventListener('markerFound', () => {
-    printDialog(e.id);
-    console.log('found' + e);
+    currentMarker = eval(e.id);
+    console.log(currentMarker);
   });
 });
 
 
+
+
 var i = 0;
-var demoMarker = 'Te voilà enfin aventurier.ère ! Je me présente, je suis Bill, maître tisserand. Dites-moi mon brave, j’ai entendu dire que vous partiez explorer les contrées perdues de notre royaume de Serres mais de ce que je vois vous êtes nu.e comme un ver ! Écoutez, il a peut être une tunique que je peux vous fournir mais j’aurais besoin d’étoffes supplémentaires, aidez moi à les retrouvez et je vous équiperai !';
-var demoText = document.querySelector('#demo-text');
+var textile_marker = 'Te voilà enfin aventurier.ère ! Je me présente, je suis Bill, maître tisserand. Dites-moi mon brave, j’ai entendu dire que vous partiez explorer les contrées perdues de notre royaume de Serres mais de ce que je vois vous êtes nu.e comme un ver ! Écoutez, il a peut être une tunique que je peux vous fournir mais j’aurais besoin d’étoffes supplémentaires, aidez moi à les retrouvez et je vous équiperai !';
+var textileText = document.querySelector('#textile-text');
 
 var delay = 25;
-function printDialog(txtIn) {
-  var txt = eval(txtIn)
+function printDialog() {
   setInterval(() => {
     if (i < txt.length) {
-      demoText.setAttribute('value', demoText.getAttribute('value') + txt.charAt(i));
+      demoText.setAttribute('value', demoText.getAttribute('value') + currentMarker.charAt(i));
       i++;
     }
   }, delay);
